4e2154f97cd60b5065c357807ea6d840fdbdbf50
rhkddbs2714 <rhkddbs2714@kookmin.ac.kr>
2026-01-29 14:02:19 +0900

    choose

--- asdf.py
+++ asdf.py
@@ -1,7 +1,12 @@
-a = input("1~10까지의 숫자 중 하나를 골라보세요 : ")
+while True:
+    a = int(input("1~10까지의 숫자 중 하나를 골라보세요 : "))
 
-if a > 5:
-    print("큰 숫자")
-
-else:
-    print("작은 숫자")
+    if 1 <= a <= 10:
+        if a > 5:
+            print("큰 숫자")
+        else:
+            print("작은 숫자")
+        break
+    
+    else:
+        print("1~10 사이의 숫자만 입력하세요")
